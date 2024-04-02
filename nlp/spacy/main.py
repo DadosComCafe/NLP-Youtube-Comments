6ec606f5f61829075c9ede264b3172daae036521
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-04-02 19:01:21 -0300

    (nlp/spacy/main.py): Add function to generate the token by spacy

--- nlp/spacy/main.py
+++ nlp/spacy/main.py
@@ -1,5 +1,5 @@
 import logging
-from spacy import cli, load, util
+from spacy import cli, load, util, tokens
 
 def get_model(model_name: str = "pt_core_news_md") -> bool:
     """Get the model if it there isn't in python environment.
@@ -24,7 +24,22 @@ def get_model(model_name: str = "pt_core_news_md") -> bool:
         except Exception as e:
             raise Exception(f"An error: {e}!")
 
+def generate_token(text: str, model_name: str = "pt_core_news_md") -> tokens.doc.Doc:
+    """Generates the token that will make possible to do an analyses above the given `text`. 
+
+    Args:
+        text (str): The text you want to analyze. 
+        model_name (str, optional): The model used in spacy to get the text as token. Defaults to "pt_core_news_md".
+
+    Returns:
+        tokens.doc.Doc: The given `text` as a token object.
+    """
+    get_model(model_name=model_name)
+    nlp = load(model_name)
+    doc = nlp(text)
+    return doc
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    get_model()
+    generate_token("A very simple text!")
